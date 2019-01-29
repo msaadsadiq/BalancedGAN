68fc1bfce6e12d0864d525ab1f0771fc01e205db
Saad Sadiq <msaadsadiq@gmail.com>
2019-01-29 13:40:04 -0500

    Update resize.sh

--- dataset/resize.sh
+++ dataset/resize.sh
@@ -4,9 +4,9 @@
 #also reformats everything from whatever to png
 
 
-if [ `ls dataset/Flood_final/*/*/*.png 2> /dev/null | wc -l ` -gt 0 ]; then
+if [ `ls CIFAR/*/*/*.png 2> /dev/null | wc -l ` -gt 0 ]; then
   echo hi
-  for file in dataset/Flood_final/*/*/*.png; do
+  for file in CIFAR/*/*/*.png; do
     convert "$file" -resize 128x128\! "${file%.*}.png"
     file "$file" #uncomment for testing
   done
